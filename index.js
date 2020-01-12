7d86b28de488248122169be89e8cad3a32ea1ed1
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-01-12 19:55:05 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,115 @@
+'use strict'
+require('shelljs/global');
+const fs = require('fs')
+const path = require('path')
+const JSON_EXT = '.report.json'
+const HTML_EXT = '.report.html'
+
+function execute(options) {
+  log = log.bind(log, options.verbose || false)
+
+  const out = `./report/lighthouse/${options.name}`
+  const lhScript = lighthouseScript(options, log)
+  
+  rm('-rf', out)
+  mkdir('-p', out)
+
+  const count = options.sites.length
+  log(`Lighthouse batch run begin for ${count} site${count > 1 ? 's' : ''}`)
+
+  const runLighthouse = (run) => {
+
+    const iSites = sitesInfo(options);
+    iSites.map((site, i) => {
+        console.log(`This is my turn ${run}`);
+        const prefix = `${i + 1}/${count}: `
+        const htmlOut = options.html ? ' --output html' : ''
+        const finalOutputFolder = path.join(out, String(run))
+        mkdir('-p', finalOutputFolder)
+        const filePath = path.join(finalOutputFolder, site.file)
+        
+        const customParams = options.params || ''
+        const chromeFlags = customParams.indexOf('--chrome-flags=') === -1 ? `--chrome-flags="--no-sandbox --headless --disable-gpu"` : ''
+        const outputPath = options.html ? filePath.slice(0, -JSON_EXT.length) : filePath
+        
+        console.log(outputPath);
+
+        const cmd = `"${site.url}" --output json${htmlOut} --output-path "${outputPath}" ${chromeFlags} ${customParams}`
+
+        log(`${prefix}Lighthouse analyzing '${site.url}'`)
+        log(cmd)
+        
+        const outcome = exec(`${lhScript} ${cmd}`)
+    });
+
+  };
+  
+  log(`Lighthouse batch run end`)
+
+  for(let i=0; i < options.runs; i++){
+    runLighthouse(i)
+  } 
+}
+
+function sitesInfo(options) {
+  let sites = []
+  if (options.file) {
+    try {
+      const contents = fs.readFileSync(options.file, 'utf8')
+      sites = contents.trim().split('\n')
+    } catch (e) {
+      console.error(`Failed to read file ${options.file}, aborting.\n`, e)
+      process.exit(1)
+    }
+  }
+  if (options.sites) {
+    sites = sites.concat(options.sites)
+  }
+  return sites.map(url => {
+    url = url.trim()
+    if (!url.match(/^https?:/)) {
+      if (!url.startsWith('//')) url = `//${url}`
+      url = `https:${url}`
+    }
+    const name = siteName(url)
+    const info = {
+      url,
+      name,
+      file: `${name}${JSON_EXT}`
+    }
+    if (options.html) info.html = `${name}${HTML_EXT}`
+    return info
+  })
+}
+
+function lighthouseScript(options, log) {
+  if (options.useGlobal) {
+    if (exec('lighthouse --version').code === 0) {
+      log('Targeting global install of Lighthouse cli')
+      return 'lighthouse'
+    } else {
+      console.warn('Global Lighthouse install not found, falling back to local one')
+    }
+  }
+  let cliPath = path.resolve(`${__dirname}/node_modules/lighthouse/lighthouse-cli/index.js`)
+  if (!fs.existsSync(cliPath)) {
+    cliPath = path.resolve(`${__dirname}/../lighthouse/lighthouse-cli/index.js`)
+    if (!fs.existsSync(cliPath)) {
+      console.error(`Failed to find Lighthouse CLI, aborting.`)
+      process.exit(1)
+    }
+  }
+  log(`Targeting local Lighthouse cli at '${cliPath}'`)
+  return `node ${cliPath}`
+}
+
+function siteName(site) {
+  return site.replace(/^https?:\/\//, '').replace(/[\/\?#:\*\$@\!\.]/g, '_')
+}
+
+
+function log(v, msg) {
+  if (v) console.log(msg)
+}
+
+module.exports = execute;
---
+++ run.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+'use strict'
+
+const program = require('commander')
+const execute = require('./index')
+
+program
+  .version(require('./package.json').version)
+  .option('-s, --sites [sites]', 'a comma delimited list of site urls to analyze with Lighthouse', (str) => str.split(','), [])
+  .option('-f, --file [path]', 'an input file with a site url per-line to analyze with Lighthouse')
+  .option('-p, --params <params>', 'extra parameters to pass to lighthouse cli for each execution e.g. -p "--perf --quiet"')
+  .option('-h, --html', 'generate an html report alongside the json report')
+  .option('-o, --out [out]', `the output folder to place reports, defaults to '${execute.OUT}'`)
+  .option('-g, --use-global', 'use a global lighthouse install instead of the dependency version')
+  .option('-v, --verbose', 'enable verbose logging')
+  .option('-r, --runs [runs]', 'number of runs')
+  .option('-n, --name [name]', 'folder name')
+  .parse(process.argv)
+
+execute(program)
