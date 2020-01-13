cd3f025763e2c64faa1a509b58a21fa325824855
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-01-13 12:55:07 +0000

    WIP

--- index.js
+++ index.js
@@ -1,108 +1,193 @@
-'use strict'
-require('shelljs/global');
-const fs = require('fs')
-const path = require('path')
-const JSON_EXT = '.report.json'
-const HTML_EXT = '.report.html'
+"use strict";
+require("shelljs/global");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+
+const func = filenames => {
+  return Promise.all(filenames.map(f => fsPromises.readFile(f)));
+};
+
+Array.prototype.sum = function () {
+  return this.reduce(function (a, b) {
+    return a + b;
+  });
+};
+
+const path = require("path");
+const JSON_EXT = ".report.json";
+const HTML_EXT = ".report.html";
 
 function execute(options) {
-  log = log.bind(log, options.verbose || false)
+  log = log.bind(log, options.verbose || false);
 
-  const out = `./report/lighthouse/${options.name}`
-  const lhScript = lighthouseScript(options, log)
-  
-  rm('-rf', out)
-  mkdir('-p', out)
+  const out = `./report/lighthouse/${options.name}`;
+  const lhScript = lighthouseScript(options, log);
 
-  const runLighthouse = (run) => {
+  rm("-rf", out);
+  mkdir("-p", out);
+  const runLighthouse = run => {
+    const count = options.sites.length;
+    log(
+      `Lighthouse batch run begin for ${count} site${
+        count > 1 ? "s" : ""
+      } current run is: ${run}`
+    );
 
-    const count = options.sites.length
-    log(`Lighthouse batch run begin for ${count} site${count > 1 ? 's' : ''} current run is: ${run}`)
-    
     sitesInfo(options).map((site, i) => {
-        const prefix = `${i + 1}/${count}: `
-        const htmlOut = options.html ? ' --output html' : ''
-        const finalOutputFolder = path.join(out, String(run))
-        mkdir('-p', finalOutputFolder)
-        const filePath = path.join(finalOutputFolder, site.file)
-        const customParams = options.params || ''
-        const chromeFlags = customParams.indexOf('--chrome-flags=') === -1 ? `--chrome-flags="--no-sandbox --headless --disable-gpu"` : ''
-        const outputPath = options.html ? filePath.slice(0, -JSON_EXT.length) : filePath
-        const cmd = `"${site.url}" --output json${htmlOut} --output-path "${outputPath}" ${chromeFlags} ${customParams}`
+      const prefix = `${i + 1}/${count}: `;
+      const htmlOut = options.html ? " --output html" : "";
+      const finalOutputFolder = path.join(out, String(run));
+      mkdir("-p", finalOutputFolder);
+      const filePath = path.join(finalOutputFolder, site.file);
+      const customParams = options.params || "";
+      const chromeFlags =
+        customParams.indexOf("--chrome-flags=") === -1 ?
+        `--chrome-flags="--no-sandbox --headless --disable-gpu"` :
+        "";
+      const outputPath = options.html ?
+        filePath.slice(0, -JSON_EXT.length) :
+        filePath;
+      const cmd = `"${site.url}" --output json${htmlOut} --output-path "${outputPath}" ${chromeFlags} ${customParams}`;
 
-        log(`${prefix}Lighthouse analyzing '${site.url}'`)
-        log(cmd)
-        
-        const outcome = exec(`${lhScript} ${cmd}`)
+      log(`${prefix}Lighthouse analyzing '${site.url}'`);
+      log(cmd);
+
+      const outcome = exec(`${lhScript} ${cmd}`);
     });
-
   };
-  
-  for(let i=0; i < options.runs; i++){
-    runLighthouse(i)
-  } 
 
-  log(`Lighthouse batch run end`)
+  for (let i = 0; i < options.runs; i++) {
+    // runLighthouse(i)
+  }
+
+  log(`Lighthouse batch run end`);
+  readFiles("jimstoik13", options.runs);
 }
 
+const readFiles = (name, runs) => {
+  const averages = [];
+
+  const pathR = `./report_test/lighthouse/${name}/0/`;
+
+  fs.readdir(pathR, function (err, files) {
+
+    files.forEach(function (file) {
+      console.log(`Running for File ${file}`);
+
+      func([
+          `./report_test/lighthouse/jimstoik13/0/${file}`,
+          `./report_test/lighthouse/jimstoik13/1/${file}`,
+          `./report_test/lighthouse/jimstoik13/2/${file}`
+        ])
+        .then(res => {
+          const summary = {
+            url: "",
+            performance: [],
+            accessibility: [],
+            bestPractices: [],
+            seo: [],
+            firstMeaningfulPaint: [],
+            timeToInteractive: []
+          };
+
+          res.forEach((fileBuffer, i) => {
+            const parsedFile = JSON.parse(fileBuffer.toString("utf-8"));
+
+            summary.url = parsedFile.finalUrl;
+            summary.performance.push(parsedFile.categories.performance.score);
+            summary.accessibility.push(parsedFile.categories.accessibility.score);
+            summary.bestPractices.push(parsedFile.categories["best-practices"].score);
+            summary.seo.push(parsedFile.categories.seo.score);
+            summary.firstMeaningfulPaint.push(parsedFile.audits["first-meaningful-paint"].numericValue);
+            summary.timeToInteractive.push(parsedFile.audits.interactive.numericValue);
+          });
+
+          const avg = {
+            url: summary.url,
+            performanceAvg: (summary.performance.sum() / summary.performance.length).toFixed(2),
+            accessibilityAvg: (summary.accessibility.sum() / summary.accessibility.length).toFixed(2),
+            bestPracticesAvg: (summary.bestPractices.sum() / summary.bestPractices.length).toFixed(2),
+            seoAvg: (summary.seo.sum() / summary.seo.length).toFixed(2),
+            firstMeaningfulPaintAvg: (summary.firstMeaningfulPaint.sum() / summary.firstMeaningfulPaint.length).toFixed(2),
+            timeToInteractiveAvg: (summary.timeToInteractive.sum() / summary.timeToInteractive.length).toFixed(2)
+          };
+
+          averages.push(avg);
+          console.log(averages);
+
+          const summaryPath = `./report_test/lighthouse/jimstoik13/summary.json`;
+          fs.writeFileSync(summaryPath, JSON.stringify(averages, null, 4))
+
+        })
+        .catch(console.log);
+    });
+  });
+};
+
 function sitesInfo(options) {
-  let sites = []
+  let sites = [];
   if (options.file) {
     try {
-      const contents = fs.readFileSync(options.file, 'utf8')
-      sites = contents.trim().split('\n')
+      const contents = fs.readFileSync(options.file, "utf8");
+      sites = contents.trim().split("\n");
     } catch (e) {
-      console.error(`Failed to read file ${options.file}, aborting.\n`, e)
-      process.exit(1)
+      console.error(`Failed to read file ${options.file}, aborting.\n`, e);
+      process.exit(1);
     }
   }
   if (options.sites) {
-    sites = sites.concat(options.sites)
+    sites = sites.concat(options.sites);
   }
   return sites.map(url => {
-    url = url.trim()
+    url = url.trim();
     if (!url.match(/^https?:/)) {
-      if (!url.startsWith('//')) url = `//${url}`
-      url = `https:${url}`
+      if (!url.startsWith("//")) url = `//${url}`;
+      url = `https:${url}`;
     }
-    const name = siteName(url)
+    const name = siteName(url);
     const info = {
       url,
       name,
       file: `${name}${JSON_EXT}`
-    }
-    if (options.html) info.html = `${name}${HTML_EXT}`
-    return info
-  })
+    };
+    if (options.html) info.html = `${name}${HTML_EXT}`;
+    return info;
+  });
 }
 
 function lighthouseScript(options, log) {
   if (options.useGlobal) {
-    if (exec('lighthouse --version').code === 0) {
-      log('Targeting global install of Lighthouse cli')
-      return 'lighthouse'
+    if (exec("lighthouse --version").code === 0) {
+      log("Targeting global install of Lighthouse cli");
+      return "lighthouse";
     } else {
-      console.warn('Global Lighthouse install not found, falling back to local one')
+      console.warn(
+        "Global Lighthouse install not found, falling back to local one"
+      );
     }
   }
-  let cliPath = path.resolve(`${__dirname}/node_modules/lighthouse/lighthouse-cli/index.js`)
+  let cliPath = path.resolve(
+    `${__dirname}/node_modules/lighthouse/lighthouse-cli/index.js`
+  );
   if (!fs.existsSync(cliPath)) {
-    cliPath = path.resolve(`${__dirname}/../lighthouse/lighthouse-cli/index.js`)
+    cliPath = path.resolve(
+      `${__dirname}/../lighthouse/lighthouse-cli/index.js`
+    );
     if (!fs.existsSync(cliPath)) {
-      console.error(`Failed to find Lighthouse CLI, aborting.`)
-      process.exit(1)
+      console.error(`Failed to find Lighthouse CLI, aborting.`);
+      process.exit(1);
     }
   }
-  log(`Targeting local Lighthouse cli at '${cliPath}'`)
-  return `node ${cliPath}`
+  log(`Targeting local Lighthouse cli at '${cliPath}'`);
+  return `node ${cliPath}`;
 }
 
 function siteName(site) {
-  return site.replace(/^https?:\/\//, '').replace(/[\/\?#:\*\$@\!\.]/g, '_')
+  return site.replace(/^https?:\/\//, "").replace(/[\/\?#:\*\$@\!\.]/g, "_");
 }
 
 function log(v, msg) {
-  if (v) console.log(msg)
+  if (v) console.log(msg);
 }
 
 module.exports = execute;
